3ef5f7b7e7da2266986c578312f6a32582f1a21b
Ghost-die <wangqun1436@gmail.com>
2024-02-20 15:43:34 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ import { withContentlayer } from "next-contentlayer";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	// experimental: {
-	// 	mdxRs: true,
-	// },
-	output: 'export', // 添加此行配置来替代 next export 命令
+	experimental: {
+		mdxRs: true,
+	},
+	// output: 'export', // 添加此行配置来替代 next export 命令
 };
 export default withContentlayer(nextConfig);
