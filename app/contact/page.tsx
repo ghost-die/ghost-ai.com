9c95076bb5c255d5f6f2a4490ea1c64fff25e402
Ghost-die <wangqun1436@gmail.com>
2024-03-06 16:15:00 +0800

    email

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,17 +5,11 @@ import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
-	// {
-	// 	icon: <Twitter size={20} />,
-	// 	href: "https://twitter.com/chronark_",
-	// 	label: "Twitter",
-	// 	handle: "@chronark_",
-	// },
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:wangqun1436@gmail.com",
+		href: "mailto:ghost@ghost-ai.com",
 		label: "Email",
-		handle: "wangqun1436@gmail.com",
+		handle: "ghost@ghost-ai.com",
 	},
 	{
 		icon: <Github size={20} />,
